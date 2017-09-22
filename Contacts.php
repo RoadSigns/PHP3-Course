af4fb523ca8ea1bf571e1ccbd698bc30563ea4dd
Zack <zack@zacklott.co.uk>
2017-09-22 22:08:14 +0100

    EDIT: Remove Function within Contacts
    I took another look into the Contacts class and how I could add more funcationality within it.
    
    I first added the removeContact() function that allows users to unset objects they have created.
    
    Afterwards I wanted to create a listContacts that would provide the user with information about their contacts list in JSON. However after trying to encode the Object with json_encode() I noticed that Objects with Objects within them were unable to be displayed. This means I will have to change how I store the contacts information into a key value array so the information can be encoded.
    
    Once I've got this working I place on making the listContacts() function have JSON and XML returning depending on the users request.
    
    [+] addContact()

--- Contacts.php
+++ Contacts.php
@@ -1,43 +1,63 @@
 <?php
 
+    include 'dumpr.php';
+
 class ContactBook 
 {
-    public $list;
     public function __construct()
     {
-        $this->list = new stdClass();
     }
     
     public function addContact($firstName, $surname, $dateOfBirth) 
     {
-        $contact = $firstName . " " . $surname;
+        $contact = $firstName . "" . $surname;
         $contactDetails = new Contact($firstName, $surname, $dateOfBirth);
-        $this->list->$contact = $contactDetails;
+        $this->$contact = $contactDetails;
     }
+
+    public function removeContact($contact)
+    {
+        unset($this->$contact);
+    }
+
+
+    /*
+     *
+     * Currently I am unable to turn Objects with Objects within them into JSON using json_encode()
+     * Might have to turn details within the Contact Object into an key => value array to allow the encoding to work easier
+     * This would mean I might have to loop out all the results if I want to use them information going forward
+     *
+     */
+    public function listContacts()
+    {
+        return $this;
+    }
+
+
 }
 
 class Contact 
 {
-    protected $firstName;
-    protected $surname;
     protected $fullName;
     protected $dateOfBirth;
     protected $age;
     
     public function __construct($firstName, $surname, $dateOfBirth)
     {
-        $this->firstName   = $firstName;
-        $this->surname     = $surname;
         $this->fullName    = $firstName . " " . $surname;
         $this->dateOfBirth = date("d F Y", strtotime($dateOfBirth));
     }
-    
 }
 
 $contactList = new ContactBook;
 
 $contactList->addContact("Zack", "Lott", "1993-04-14");
+$contactList->addContact("Jacquizz", "Rodgers", "1989-12-14");
+$contactList->addContact("Jared", "Goff", "1991-08-12");
+$contactList->addContact("Ling", "Chan", "1995-10-01");
 
-echo "<pre>";
-    var_dump($contactList);
-echo "</pre>";
+dumpr($contactList);
+
+$contactList->removeContact("ZackLott");
+
+dumpr($contactList);
